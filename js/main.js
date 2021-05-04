d866f28044434e514b7db91a00b4822bb108eeb9
Aayush Kashyap <kashyapaayush32@gmail.com>
2021-05-04 20:43:48 +0530

    minor changes

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@
 		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
 		var expires = "expires=" + d.toUTCString();
 		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-		console.log(document.cookie);
+		//console.log(document.cookie);
 	}
 
 	function getCookie(cname) {
